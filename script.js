d6936ec3b382150f7cf6a63f3a32dbf2f2a25494
Alexjeii <alexandre.millet59540@gmail.com>
2022-09-19 11:00:41 +0100

    ex fruits

---
+++ script.js
@@ -0,0 +1,18 @@
+let ville="cambrai";
+const hypotenuse=8;
+let admission="true";
+let nom="millet";
+let prenom="alex";
+let age=27;
+
+let pommes=4;
+let peches=6;
+let TotalFruits=pommes*peches;
+
+console.log(TotalFruits);
+
+let a=20;
+let b=4;
+let total=a/b;
+
+console.log(total);
